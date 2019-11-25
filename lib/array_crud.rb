5d81bea2503aa615d7084647450586101d52afe8
Ruari Day <ruariday@gmail.com>
2019-11-25 15:12:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   family_names = ["Fred","Camilla","Mattie","Rodger"]
-  family_names[0]
+  family_names[2]
 end
 
 def retrieve_first_element_from_array(array)
