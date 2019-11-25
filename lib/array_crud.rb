ec064e5f3ab8af004916ff105b4c12778bc44fba
Ruari Day <ruariday@gmail.com>
2019-11-25 14:50:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  family_names << ["Ruari"]
+  family_names.push "Ruari"]
 end
 
 def add_element_to_start_of_array(array, element)
